9e0bfd8ac52bfca9b18195e74e2e5bd11875536c
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-09-02 17:42:22 -0600

    Create drawBox()

--- drawBox.py
+++ drawBox.py
@@ -1,1 +1,13 @@
+def drawBox(sym, width, height): 
+    """
+    Draws a hollow rectangular box using the given symbol, width, and height.
+    """
+    # Print top border
+    print(sym * width)
 
+    # Print middle rows with spaces inside
+    for i in range(height - 2):
+        print(sym + ' ' * (width - 2) + sym)
+
+    # Print bottom border
+    print(sym * width)
